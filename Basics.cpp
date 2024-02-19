5d9b8be07eb3ac0c6120987bda54e74246633f11
Gustavo Freire <147093330+GustavoFPS@users.noreply.github.com>
2024-02-18 21:16:15 -0300

    PrimeirosPassos

---
+++ Basics.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+    /* std::cout << "Ola, mundo!"; */
+
+    /* int MyInt = 0;
+
+    MyInt = MyInt + 20;
+
+    MyInt *= 2;
+
+    std::cout << MyInt; */
+
+    /* Tipos: 1) int (inteiros)
+              2) float (float MyFloat = 0.0f, lembrar de colocar o f no final para não confundir com Double)
+              3) double (float porém com maior precisão)
+              4) bool (booleano)
+              5) char (caractere único, para string é necessário criar array)
+              6) size_t (inteiro não negativo, semelhante ao próximo tipo)
+              7) unsigned int (inteiro sem sinal, não negativo)*/
+
+    
+    return 0;
+}
